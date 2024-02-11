ce4d99bfb0cab67df18c3ce7db5ad4718d76eb16
Esupartners <149669549+Esupartners@users.noreply.github.com>
2024-02-11 23:13:54 +0100

    food segmentation module

---
+++ FoodSegmentation/sam_model.py
@@ -0,0 +1,170 @@
+import sys 
+import os
+
+from segment_anything import sam_model_registry, SamPredictor
+from segment_anything.utils.transforms import ResizeLongestSide
+from postprocessing import close_mask,open_mask
+from FoodAreaSegmentation.utils import format_bbox
+
+
+
+import numpy as np
+import torch
+import os
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def downloadSAMModel():
+    return None
+
+
+def LoadSAMPredictor(sam_checkpoint, model_type, device='cuda', return_sam=False):
+    """
+    Load a Segment Anything Model (SAM) predictor model for semantic segmentation.
+
+    Parameters:
+    - sam_checkpoint (str): The path to the checkpoint file containing the SAM model's weights and configuration.
+    - model_type (str): The SAM model type to use. It should be a key that corresponds to a model in the 'sam_model_registry'.
+    - device (str, optional): The device to run the model on, either 'cuda' (GPU) or 'cpu' (CPU). Default is 'cuda'.
+
+    Returns:
+    - predictor (SamPredictor): An instance of the SAM predictor configured with the specified model type and loaded weights.
+    """
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam.to(device=device)
+    predictor = SamPredictor(sam)
+
+    if return_sam :
+        return predictor,sam
+    else : 
+        return predictor
+
+
+def prepare_image(image, transform, device):
+    image = transform.apply_image(image)
+    image = torch.as_tensor(image, device=device.device) 
+    return image.permute(2, 0, 1).contiguous()
+
+
+def GenerateMask(predictor, input_box, include_point=True):
+    """
+    Generate segmentation masks for a given input bounding box using a SAM predictor.
+
+    Parameters:
+    - predictor (SamPredictor): An instance of the SAM predictor loaded with a model.
+    - input_box (list or numpy array): The bounding box coordinates in the format [x_min, y_min, x_max, y_max].
+
+    Returns:
+    - masks (numpy array): A segmentation mask representing the predicted object(s) within the input bounding box.
+    """
+    if include_point:
+        point_coords = np.array([[(input_box[0]+input_box[2])/2,(input_box[1]+input_box[3])/2]])
+        point_labels=np.array([1]) #label 0 is for background and label 1 is for foreground
+    else: 
+        point_coords = None
+        point_labels = None
+
+    masks, iou, _ = predictor.predict(
+        point_coords=point_coords,
+        point_labels=point_labels,  
+        box=input_box[None, :],
+        multimask_output=False,
+    )
+    return masks, iou
+
+
+def GenerateMaskBatch(sam, images, input_boxes, include_point=True):
+
+    for i in range(len(input_boxes)):
+        input_boxes[i] = torch.tensor(input_boxes[i], device=sam.device)
+
+    resize_transform = ResizeLongestSide(sam.image_encoder.img_size)
+    batched_input = [
+     {
+         'image': prepare_image(images[0], resize_transform, sam),
+         'boxes': resize_transform.apply_boxes_torch(input_boxes[0], images[0].shape[:2]),
+         'original_size': images[0].shape[:2]
+     },
+     {
+         'image': prepare_image(images[1], resize_transform, sam),
+         'boxes': resize_transform.apply_boxes_torch(input_boxes[0], input_boxes[0].shape[:2]),
+         'original_size': input_boxes[0].shape[:2]
+     }
+    ]
+
+    batched_output = sam(batched_input, multimask_output=False)
+
+    return batched_output
+
+def prepare_image_embeddings(image,model_type):
+
+    #infere model checkpoint path
+    if model_type == "vit_b":
+        sam_checkpoint = os.path.join('.','FoodAreaSegmentation','sam_vit_b_01ec64.pth')
+    elif model_type == "vit_l":
+        sam_checkpoint = os.path.join('.','FoodAreaSegmentation','sam_vit_l_0b3195.pth')
+    elif model_type == "vit_h":
+        sam_checkpoint = os.path.join('.','FoodAreaSegmentation','sam_vit_h_4b8939.pth')
+
+    #Loading SAM predictor
+    sam_predictor = LoadSAMPredictor(sam_checkpoint=sam_checkpoint,model_type=model_type,device='cpu')
+
+    #Create SAM embeddings for the image
+    sam_predictor.set_image(image)
+
+    return sam_predictor
+
+def GenerateMaskForImage(sam_predictor, bounding_boxes=[],open=False,close=False,kernel_size=None):
+
+    masks=[]
+
+    iou_predictions = []
+
+    #Generate mask
+    if len(bounding_boxes) == 1 :
+
+        if type(bounding_boxes[0]) == dict:
+            input_bbox = np.array(format_bbox(bounding_boxes[0]['bbox']))
+        else:
+            input_bbox = np.array(format_bbox(bounding_boxes[0]))
+
+        mask, iou = GenerateMask(sam_predictor,input_box=input_bbox)
+        iou_predictions.append(iou)
+        if open and not(close):
+            mask[0] = open_mask(np.array(mask[0]*1,dtype=np.uint8))
+            masks.append(mask)
+        elif close and not(open):
+            mask[0] = close_mask(np.array(mask[0]*1,dtype=np.uint8))
+            masks.append(mask)
+        elif open and close:
+            mask[0] = close_mask(close_mask(np.array(mask[0]*1,dtype=np.uint8)))
+            masks.append(mask)
+        else:
+            masks.append(mask)
+    elif len(bounding_boxes) > 1 :
+        for i in range(len(bounding_boxes)):
+
+            if type(bounding_boxes[i]) == dict:
+                input_bbox = np.array(format_bbox(bounding_boxes[i]['bbox']))
+            else:
+                input_bbox = np.array(format_bbox(bounding_boxes[i]))
+
+            mask, iou = GenerateMask(sam_predictor,input_box=input_bbox)
+            iou_predictions.append(iou)
+            if open and not(close):
+                mask[0] = open_mask(np.array(mask[0]*1,dtype=np.uint8),kernel_size=kernel_size)
+                masks.append(mask)
+            elif close and not(open):
+                mask[0] = close_mask(np.array(mask[0]*1,dtype=np.uint8),kernel_size=kernel_size)
+                masks.append(mask)
+            elif open and close:
+                mask[0] = open_mask(close_mask(np.array(mask[0]*1,dtype=np.uint8),kernel_size=kernel_size))
+                masks.append(mask)
+            else:
+                masks.append(mask)
+
+    return masks,iou_predictions
+
+
+
---
+++ FoodSegmentation/utils.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
+def show_box(box, ax, iou=0, category_name=None):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
+    if category_name is not None:
+        ax.text(x0, y0 - 5, category_name, color='green', fontsize=12, backgroundcolor='black')
+    if iou is not None:
+        ax.text(x0, y0 + h + 5, f'IoU: {iou:.2f}', color='red', fontsize=12, backgroundcolor='black')
+
+def format_bbox(input_bbox):
+    #format the bounding box values to sam model bbox specifications
+    return [input_bbox[0]-int(input_bbox[2]/2), input_bbox[1]-int(input_bbox[3]/2), input_bbox[0]+int(input_bbox[2]/2), input_bbox[1]+int(input_bbox[3]/2)]
+
+def show_mask_cv2(mask, image, random_color=False):
+    if random_color:
+        color = np.random.rand(3) * 255
+    else:
+        color = (255, 0, 0)
+
+    mask_image = np.zeros_like(image)
+    mask_image[:, :, 0] = mask * color[0]
+    mask_image[:, :, 1] = mask * color[1]
+    mask_image[:, :, 2] = mask * color[2]
+
+    image = cv2.addWeighted(image, 1, mask_image, 0.7, 0)
+
+    return image
+
+def show_points_cv2(coords, labels, image, marker_size=375):
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    
+    for point in pos_points:
+        cv2.drawMarker(image, (int(point[0]), int(point[1])), (0, 255, 0), markerType=cv2.MARKER_STAR, markerSize=marker_size, thickness=2, line_type=cv2.LINE_AA)
+    
+    for point in neg_points:
+        cv2.drawMarker(image, (int(point[0]), int(point[1])), (0, 0, 255), markerType=cv2.MARKER_STAR, markerSize=marker_size, thickness=2, line_type=cv2.LINE_AA)
+    
+    return image
+
+def show_box_cv2(box, image, iou=0, category_name=None):
+    x0, y0, x1, y1 = map(int, box)
+    image = cv2.rectangle(image, (x0, y0), (x1, y1), (0, 255, 0), 2)
+    
+    if category_name is not None:
+        cv2.putText(image, category_name, (x0, y0 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    
+    if iou is not None:
+        cv2.putText(image, f'IoU: {iou:.2f}', (x0, y1 + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    
+    return image
